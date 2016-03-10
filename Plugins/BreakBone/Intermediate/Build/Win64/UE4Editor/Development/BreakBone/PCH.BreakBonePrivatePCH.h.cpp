de63d128040bbbba88de505ff0c5bb12f096e0dd
Madison Wills <b.m.wills7@gmail.com>
2016-03-10 14:04:43 +1100

    Modified ruth limbs constraints, added materials to Ruth and AEIOU's limbs.

--- Plugins/BreakBone/Intermediate/Build/Win64/UE4Editor/Development/BreakBone/PCH.BreakBonePrivatePCH.h.cpp
+++ Plugins/BreakBone/Intermediate/Build/Win64/UE4Editor/Development/BreakBone/PCH.BreakBonePrivatePCH.h.cpp
@@ -1,1 +1,1 @@
-#include "C:\Users\anthony.bogli\Desktop\ToyBoxBrawlUE4\Plugins\BreakBone\Source\BreakBone\Private\BreakBonePrivatePCH.h"
+#include "C:\Users\brock.wills\Documents\Unreal Projects\ToyBoxBrawlUE4\Plugins\BreakBone\Source\BreakBone\Private\BreakBonePrivatePCH.h"
