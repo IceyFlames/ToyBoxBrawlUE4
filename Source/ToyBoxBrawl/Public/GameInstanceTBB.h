62bd9fe45c9e4add9241a3875763b8912f4873ec
Anthony Bogli <anthonybogli1@hotmail.com>
2016-04-01 18:35:40 +1100

    You can now select hair colour and Torso colour in the lobby menu widget, also c++ the game instance so that the information carries over to the main map

---
+++ Source/ToyBoxBrawl/Public/GameInstanceTBB.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/GameInstance.h"
+#include "GameInstanceTBB.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOYBOXBRAWL_API UGameInstanceTBB : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
