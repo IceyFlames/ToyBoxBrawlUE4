7ac1a8634637b7d88cc798db5dfd80cf5a4e1f8e
Madison Wills <b.m.wills7@gmail.com>
2016-04-01 15:30:08 +1100

    Updated to 4.11 New inputs added Fixed inputs for Mannequin, AEIOU and Ruth Moved all widgets to the Blueprints/Widgets Hair_base material made Main menu updated to use new graphics, removed players 5 - 8.

--- Plugins/BreakBone/Intermediate/Build/Win64/UE4/Development/BreakBone/PCH.BreakBonePrivatePCH.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\anthony.bogli\Desktop\ToyBoxBrawlUE4\Plugins\BreakBone\Source\BreakBone\Private\BreakBonePrivatePCH.h"
--- Plugins/BreakBone/Intermediate/Build/Win64/UE4Editor/Development/BreakBone/PCH.BreakBonePrivatePCH.h.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:\Users\brock.wills\Documents\Unreal Projects\ToyBoxBrawlUE4\Plugins\BreakBone\Source\BreakBone\Private\BreakBonePrivatePCH.h"
