275476f35125af50333eadff0b97b8e93df9a43f
Anthony Bogli <anthonybogli1@hotmail.com>
2016-04-04 10:58:43 +1000

    Rest in peace, forever in my heart, greybox assets folder

--- Source/ToyBoxBrawl/Public/GameInstanceTBB.h
+++ Source/ToyBoxBrawl/Public/GameInstanceTBB.h
@@ -8,12 +8,49 @@
 /**
  * 
  */
+
+USTRUCT(BlueprintType)
+struct FPlayerCharacter
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	int32 _ControllerID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	FString _CharacterID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	int32 _TorsoColour;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	int32 _Haircolour;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	//int32 _TorsoID;
+	//
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	//int32 _PantsID;
+
+};
+
+
+
 UCLASS()
 class TOYBOXBRAWL_API UGameInstanceTBB : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerGameInstance")
+	TArray<FPlayerCharacter> _Players;
 	
-	
-	
-	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	int32 _PlayerNum = 2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PlayerCharacter")
+	int32 _PlayersDead;
+
+private:
+
 };
